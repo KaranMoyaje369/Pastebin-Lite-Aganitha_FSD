c6aac77ddc11048c23c15601527d84666deed420
Karan Moyaje <karanmoyaje369@gmail.com>
2025-12-30 23:43:32 +0530

    Add favicon to fix 404 error

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Pastebin-Lite',
   description: 'A simple pastebin application',
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">📝</text></svg>',
+  },
 }
 
 export default function RootLayout({
